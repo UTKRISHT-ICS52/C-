b010a6bf0e827045f31a3d6beb349e2af9f4e246
UTKRISHT-ICS52 <codewithutkrisht@gmail.com>
2025-09-02 14:48:08 +0530

    C programmming Code

---
+++ Hello.c
@@ -0,0 +1,23 @@
+/*
+// C Programming code 
+#include<stdio.h>
+int main(){
+int a[5]= {2,3,4,5,6} ;
+for(int i=0;i<5; i++){
+printf(" a[%d]=%d\n", i, a[i]);
+}
+return 0;
+}
+
+#include <stdio.h>
+
+int main() {
+    float fahrenheit, celsius;
+    printf("Enter temperature in Fahrenheit: ");
+    scanf("%f", &fahrenheit);
+    celsius = (fahrenheit - 32) * 5 / 9;
+    printf("Temperature in Celsius: %.2f\n", celsius);
+    return 0; 
+}
+*/
+
